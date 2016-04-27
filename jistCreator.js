c84c644cd1cd532a0024859e8ae6f71c1892b3ff
Dhruv Bhavsar <dhruvhbhavsar@gmail.com>
2016-04-27 03:10:12 +0000

    Successfully Building Script based on UI input. TODO: Add favorite applications and github autoprocess

--- jistCreator.js
+++ jistCreator.js
@@ -44,9 +44,9 @@ function createGist(token) {
 var queryResponse = null;
 function randomApiSearch(query) {
     var deferred = $.Deferred();
-    var url = "https://chocolatey.org/api/v2/Search()?$filter=IsLatestVersion&$orderby=Id&$skip=0&$top=10&searchTerm='" + query + "'&targetFramework=''&includePrerelease=false";
-
-    var yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from xml where url="' + url + '"') + '&format=xml&callback=?';
+    // var url = "https://chocolatey.org/api/v2/Search()?$filter=IsLatestVersion&$orderby=Id&$skip=0&$top=50&searchTerm='" + encodeURIComponent(query) + "'&targetFramework=''&includePrerelease=false";
+    var url = "https://chocolatey.org/api/v2/Packages()?$filter=(IsPrerelease%20eq%20false%20or%20IsPrerelease%20eq%20false)%20and%20(IsLatestVersion%20or%20IsAbsoluteLatestVersion)%20and%20(substringof('" + encodeURIComponent(query) + "',Id)%20or%20substringof('" + encodeURIComponent(query) + "',Title))&$skip=0&$top=50"
+    var yql = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from xml where url="' + url + '"') + '&format=xml&callback=?';
 
     // Request that YSQL string, and run a callback function.
     // Pass a defined function to prevent cache-busting.
@@ -55,21 +55,73 @@ function randomApiSearch(query) {
         queryResponse = x2js.xml_str2json(data.results[0]);
         deferred.resolve(queryResponse);
         console.log(queryResponse);
-        var template = Handlebars.compile($('#search').html());
-        var html = template(queryResponse);
-        $('#results').html(html);
+        compileHtml();
     });
 
     return deferred;
 }
-function searchQuery(){
+function compileHtml() {
+    var template = Handlebars.compile($('#search').html());
+    var html = template(queryResponse);
+    $('#results').html(html);
+}
+function searchQuery() {
+    $("#results").html('<p class="text-center"><i class="fa fa-spinner fa-spin fa-5x fa-fw margin-bottom"></i><span class="sr-only">Loading...</span></p>');
     randomApiSearch($("#searchQuery").val());
 }
 var currentPrograms = []
-function addToInstallScript(item){
-    currentPrograms.push(item);
+function addToInstallScript(item, index) {
+    var found = currentPrograms.indexOf(item);
+    if (found > -1) {
+        currentPrograms.splice(found, 1);
+    }else{
+        currentPrograms.push(item);
+    }
+    $("#searchResult" + index).html(checkInstall(item, null))
+    updateCodeDisplay();
     console.log(currentPrograms);
 }
+function updateCodeDisplay(){
+    var buffer = "";
+    buffer += "# Template borrowed from http://blog.zerosharp.com/provisioning-a-new-development-machine-with-boxstarter/\n";
+    buffer += "# Boxstarter options\n";
+    buffer += "$Boxstarter.RebootOk=$true # Allow reboots?\n";
+    buffer += "$Boxstarter.NoPassword=$false # Is this a machine with no login password?\n";
+    buffer += "$Boxstarter.AutoLogin=$true # Save my password securely and auto-login after a reboot\n";
+    $.each(currentPrograms, function(index, value){
+        buffer += "choco install " + value + "\n";
+    })  
+    $("#autoGenerator").text(buffer)
+}
+$(document).ready(function () {
+    $('#searchQuery').keypress(function (e) {
+        if (e.which == 13) {
+            searchQuery();
+            return false;    //<---- Add this line
+        }
+    });
+});
+
+Handlebars.registerHelper('imageProxy', function (context, options) {
+    var url = "www.acsu.buffalo.edu/~rslaine/imageNotFound.jpg"
+    if (context) {
+        url = context.replace("http://", "");
+        url = url.replace("https://", "");
+    }
+    return "https://images.weserv.nl/?url=" + url + "&w=100&h=100&t=square&a=center&t=fitup";
+});
+function checkInstall(context, options) {
+    var check = '<i class="fa fa-check-square-o" aria-hidden="true"></i>';
+    var ret = '<i class="fa fa-square-o" aria-hidden="true"></i>';
+    $.each(currentPrograms, function (index, value) {
+        if (context == value) {
+            ret = check;
+        }
+    })
+    return ret;
+}
+Handlebars.registerHelper('checkInstall', checkInstall);
+
 //Using Gist ID from the response above, we edit the Gist with Ajax PATCH request
 // $.ajax({
 //     url: 'https://api.github.com/gists/GIST-ID-FROM-PREVIOUS-CALL',
