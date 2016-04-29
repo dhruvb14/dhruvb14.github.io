5e03dc995ccd6a398c0dda41944b6bf92d6b81ea
Dhruv Bhavsar <dhruvhbhavsar@gmail.com>
2016-04-29 21:08:32 +0000

    Refactor and break out files

---
+++ handlebarsExtensions.js
@@ -0,0 +1,17 @@
+Handlebars.registerHelper('imageProxy', function (context, options) {
+    var url = "www.acsu.buffalo.edu/~rslaine/imageNotFound.jpg"
+    if (context) {
+        url = context.replace("http://", "");
+        url = url.replace("https://", "");
+    }
+    return "https://images.weserv.nl/?url=" + url + "&w=100&h=100&t=square&a=center&t=fitup";
+});
+Handlebars.registerHelper('checkInstall', checkInstall);
+Handlebars.registerHelper("moduloIf", function(index_count,mod,block) {
+  if(parseInt(index_count)%(mod)=== 0){
+    return block.fn(this);}
+});
+
+Handlebars.registerHelper("DateFormatter", function(input,caller) {
+return moment(input).format("dddd, MMMM Do YYYY, h:mm:ss a"); 
+});
